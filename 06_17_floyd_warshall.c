df05d9bc65014f9766b22bc94acb48cc1db860e5
saiyedprince <saiyed.prince.7@gmail.com>
2025-10-11 10:37:49 +0530

    floyd warshall for shortest path

---
+++ 06_17_floyd_warshall.c
@@ -0,0 +1,77 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h> 
+#define V 4
+
+#define INF 99999
+
+void printSolution(int dist[][V]);
+
+void floydWarshall(int graph[][V]) {
+    int dist[V][V];
+    int i, j, k;
+
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < V; i++) {
+        if (dist[i][i] < 0) {
+            printf("Graph contains a negative weight cycle!\n");
+            return;
+        }
+    }
+
+    printSolution(dist);
+}
+
+void printSolution(int dist[][V]) {
+    printf("The following matrix shows the shortest distances between every pair of vertices:\n");
+    printf("      ");
+    for (int i = 0; i < V; i++) {
+        printf("%5d", i);
+    }
+    printf("\n");
+    printf("------");
+    for (int i = 0; i < V; i++) {
+        printf("-----");
+    }
+    printf("\n");
+
+
+    for (int i = 0; i < V; i++) {
+        printf("%5d|", i);
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%5s", "INF");
+            else
+                printf("%5d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V] = { {0,   5,  INF, 10},
+                        {INF, 0,   3,  INF},
+                        {INF, INF, 0,   1},
+                        {INF, INF, INF, 0}
+                      };
+
+    floydWarshall(graph);
+    return 0;
+}
+
