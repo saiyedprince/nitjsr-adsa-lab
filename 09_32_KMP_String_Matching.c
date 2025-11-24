46e4986f02f452b9978a5c429cbfd2c6c9e65454
saiyedprince <saiyed.prince.7@gmail.com>
2025-11-24 14:17:07 +0530

    KMP algorithm

---
+++ 09_32_KMP_String_Matching.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+
+void computeLPSArray(char* pattern, int M, int* lps) {
+    int length = 0;  
+    lps[0] = 0;      
+
+    int i = 1;
+    while (i < M) {
+        if (pattern[i] == pattern[length]) {
+            length++;
+            lps[i] = length;
+            i++;
+        } else {
+            if (length != 0) {
+                length = lps[length - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+void KMPSearch(char* pattern, char* text) {
+    int M = strlen(pattern);
+    int N = strlen(text);
+
+    int lps[M];
+
+    computeLPSArray(pattern, M, lps);
+
+    int i = 0; 
+    int j = 0; 
+    while (i < N) {
+        if (pattern[j] == text[i]) {
+            i++;
+            j++;
+        }
+
+        if (j == M) {
+            printf("Found pattern at index %d\n", i - j);
+            j = lps[j - 1];
+        } else if (i < N && pattern[j] != text[i]) {
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i++;
+        }
+    }
+}
+
+int main() {
+    char text[] = "ABABDABACDABABCABAB";
+    char pattern[] = "ABABCABAB";
+
+    printf("Text: %s\n", text);
+    printf("Pattern: %s\n", pattern);
+
+    KMPSearch(pattern, text);
+
+    return 0;
+}
